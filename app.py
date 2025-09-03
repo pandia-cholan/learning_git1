b9429412b29af9fd550d448301bc285e60d6296a
pandia-cholan <surya.m@vivyacorp.com>
2025-09-03 16:08:12 +0530

    just re push the code

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-
+#sfklajsldkfjaslkfjlksjflksjflksdjflksjflkjslkfjlsdfjsldjl
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ComputerHardwareInventory.settings')
