a312eacec56693f405016743e000fa631fb7a74e
Surya Pandi M <surya.m@vivyacorp.com>
2025-09-04 09:58:00 +0530

    single file commit

--- app.py
+++ app.py
@@ -3,7 +3,6 @@
 import os
 import sys
 
-#sfklajsldkfjaslkfjlksjflksjflksdjflksjflkjslkfjlsdfjsldjl
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ComputerHardwareInventory.settings')
